62c5c3024549db181bcce67f137b8e9bbbe2dc6b
Calli Crase <callilogancrase@gmail.com>
2019-08-05 18:00:39 -0600

    missing add inv. in manager and correct join in supervisor

---
+++ amazonManager.js
@@ -0,0 +1,140 @@
+//libraries
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const colors = require('colors');
+const Table = require('cli-table');
+//find library that formats query results in table 
+
+//sql connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "amazon_db"
+});
+connection.connect(function(err){
+    if(err) throw err;
+    console.log("connected");
+    start();
+});
+
+function start(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'userChoice',
+            message: 'What would you like to view?',
+            choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
+        }
+    ]).then(choice => {
+        switch(choice.userChoice){
+            case 'View Products for Sale':
+                viewProducts();
+            break;
+            case 'View Low Inventory':
+                viewInventory();
+            break;
+            case 'Add to Inventory':
+                addInventory();
+            break;
+            case 'Add New Product':
+                newProduct();
+            break;
+            default:
+                console.log('Error: Pick another option');
+        }
+    });
+};
+
+function viewProducts(){
+    connection.query("SELECT item_id, product_name, price, stock_quantity FROM products;", function(err, res, fields){
+        if(err) throw err;
+        const table = new Table({
+            head: ['Item ID'.yellow,'Product Name'.yellow, 'Price'.yellow, 'Quantity'.yellow]
+        });
+        for(var i = 0; i < res.length; i++){
+            table.push([res[i].item_id, res[i].product_name, res[i].price, res[i].stock_quantity]);
+        };
+        console.log(table.toString());
+        connection.end();
+    })
+};
+
+function viewInventory(){
+    connection.query('SELECT item_id, product_name, stock_quantity FROM products WHERE stock_quantity <= 5;', function(err, res, fields){
+        if(err) throw err;
+        const table = new Table({
+            head: ['Item ID'.yellow,'Product Name'.yellow, 'Quantity'.yellow]
+        });
+        for(var i = 0; i < res.length; i++){
+            table.push([res[i].item_id, res[i].product_name, res[i].stock_quantity]);
+        };
+        console.log(table.toString());
+
+        connection.end();
+    })
+};
+
+function addInventory(){
+    let products = [];
+    connection.query('SELECT product_name FROM products;', function(err, res, fields){
+        if(err) throw err;
+        for(var i=0; i <res.length; i++){
+            products.push(res[i].product_name);
+        };
+        connection.end();
+    });
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'userChoice',
+            message: 'Which product would you like to buy inventory for?',
+            choices: products //NEED TO ACCESS VALUES FROM PRODUCTS
+        }
+    ]).then(choice => {
+        console.log(choice);
+    });
+};
+
+function newProduct(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'product_name',
+            message: 'What is the name of the product?',
+        },{
+            type: 'input',
+            name: 'department_name',
+            message: 'What department is the product in?',
+        },{
+            type: 'input',
+            name: 'price',
+            message: 'What is the price?',
+        },{
+            type: 'input',
+            name: 'stock_quantity',
+            message: 'How many are in stock?',
+        }
+    ]).then(answers => {
+        var product_name = answers.product_name;
+        var department = answers.department_name;
+        var price = parseFloat(answers.price);
+        var stock_quantity = parseInt(answers.stock_quantity);
+
+        insertInto(product_name, department, price, stock_quantity);
+    });
+
+    function insertInto(product_name, department, price, stock_quantity){
+        var sql = "INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ?";
+        var values = [
+            [product_name, department, price, stock_quantity]
+        ];
+        connection.query(sql, [values], function(err, res){
+            if(err) throw err;
+            console.log("Successful product insert.".green);
+        })
+        connection.end();
+    };
+
+};
---
+++ amazonSupervisor.js
@@ -0,0 +1,85 @@
+//libraries
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const colors = require('colors');
+const Table = require('cli-table');
+//find library that formats query results in table 
+
+//sql connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "amazon_db"
+});
+connection.connect(function(err){
+    if(err) throw err;
+    console.log("connected");
+    start();
+});
+
+function start(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'userPath',
+            message: 'What would you like to do?',
+            choices: ['View Product Sales by Department', 'Create New Department']
+        }
+    ]).then(choice => {
+        if(choice.userPath === 'View Product Sales by Department'){
+            viewSalesByDept();
+        };
+        if(choice.userPath ==='Create New Department'){
+            createNewDept();
+        }
+    });
+};
+
+function viewSalesByDept(){
+    connection.query("SELECT departments.department_id, departments.department_name, departments.over_head_costs, products.product_sales, departments.over_head_costs - products.product_sales AS total_profit FROM departments JOIN products ON departments.department_name = products.department_name;", function(err, res, fields){
+        if(err) throw err;
+        const table = new Table({
+            head: ['Dep. ID'.yellow,'Name'.yellow, 'Overhead'.yellow, 'Sales'.yellow, 'Total Profit'.yellow]
+        });
+        for(var i = 0; i < res.length; i++){
+            table.push([res[i].department_id, res[i].department_name, res[i].over_head_costs, res[i].product_sales, res[i].total_profit]);
+        };
+        console.log(table.toString());
+        connection.end();
+
+    });
+};
+
+function createNewDept(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'department_name',
+            message: 'What is the name of the new department?',
+        },{
+            type: 'input',
+            name: 'over_head_costs',
+            message: 'What are the department overhead costs?',
+        }
+    ]).then(answers => {
+        var department_name = answers.department_name;
+        var over_head_costs = parseFloat(answers.over_head_costs);
+
+        insertInto(department_name, over_head_costs);
+    });
+
+    function insertInto(department_name, over_head_costs){
+        var sql = "INSERT INTO departments (department_name, over_head_costs) VALUES ?";
+        var values = [
+            [department_name, over_head_costs]
+        ];
+        connection.query(sql, [values], function(err, res){
+            if(err) throw err;
+            console.log("Successfully created a new department.".green);
+        })
+        connection.end();
+    };
+
+};
